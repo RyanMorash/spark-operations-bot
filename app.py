8aca941659ff0cafce45e2863c2f0dfc0fe4df43
Ryan Morash <ryan@morash.me>
2023-08-16 23:37:36 -0400

    switch from spark to webex

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     See the README at https://github.com/meraki/spark-operations-bot
 '''
 import os
-from ciscosparkbot import SparkBot
+from ciscoteamsbot import TeamsBot as SparkBot
 import cico_meraki
 import cico_spark_call
 import cico_combined
